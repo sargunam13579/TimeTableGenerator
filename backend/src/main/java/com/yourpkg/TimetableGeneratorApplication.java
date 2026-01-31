31f5e180305a8230d5a6280472f068556d806f93
sargunam13579 <sargunam13579@mail.com>
2026-01-31 22:42:49 +0530

    Fix base package structure

---
+++ backend/src/main/java/com/yourpkg/TimetableGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.yourpkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimetableGeneratorApplication.class, args);
+    }
+}
