932a6973be53784cdf22bc56e157a310f269d693
pgsargunam13579 <pgsargunam13579@gmail.com>
2026-02-07 23:12:20 +0530

    add

--- backend/src/main/java/com/yourpkg/entity/College.java
+++ backend/src/main/java/com/yourpkg/entity/College.java
@@ -1,18 +1,11 @@
 package com.yourpkg.entity;
-
 import jakarta.persistence.*;
-
 @Entity
 public class College {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
     @ManyToOne
     private User user;
-
-    // getters & setters
 }
